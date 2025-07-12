45d26a5bd0fbfb9521917600dcf38f572c8a78b4
Zou Kailiang Steven <zoukailiang0@163.com>
2025-07-12 11:28:09 +0800

    add more sql for sakila.sql

--- MySQL/sakila/sakila.sql
+++ MySQL/sakila/sakila.sql
@@ -8,6 +8,8 @@ and (first_name like '%Steven%' or first_name like '%Steven%')
 limit 10
 ;
 
+select count(*) from sakila.actor; -- 200
+
 insert into sakila.actor (first_name, last_name, last_update)
 values ('Steven', 'Zou', current_timestamp);
 
@@ -17,9 +19,12 @@ select
 	a.* 
 from sakila.film_text a 
 where 1 = 1
-order by film_id desc
+order by film_id -- desc
 limit 10
+offset 5
 ;
+
+select count(*) from sakila.film_text; -- 1000
 
 
 -- ----------------------------------------temp------------------------------------------
