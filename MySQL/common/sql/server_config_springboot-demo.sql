453d6bf847a25da7a9751bb2ea45815acea6ee4f
Steven Squall <zoukailiang0@163.com>
2025-08-09 16:10:21 +0800

    add server_config table

---
+++ MySQL/common/sql/server_config_springboot-demo.sql
@@ -0,0 +1,4 @@
+insert into server_config (category, name, value1, description, enter_central_time)
+values ('ConfigServer', 'springboot-demo-SwitchFlag', 'Y', 'SwitchFlag for springboot-demo', CURRENT_TIMESTAMP);
+
+commit;
