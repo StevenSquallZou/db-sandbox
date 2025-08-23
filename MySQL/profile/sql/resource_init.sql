e18ebdd6433624e16642ebd520adb4b17debc1cb
Steven Squall <zoukailiang0@163.com>
2025-08-23 16:16:40 +0800

    create profile database and tables

---
+++ MySQL/profile/sql/resource_init.sql
@@ -0,0 +1,7 @@
+insert into resource (resource_name, enter_central_time)
+values ('Read_FilmText', CURRENT_TIMESTAMP);
+
+insert into resource (resource_name, enter_central_time)
+values ('Write_FilmText', CURRENT_TIMESTAMP);
+
+commit;
---
+++ MySQL/profile/sql/user_resource_init.sql
@@ -0,0 +1,7 @@
+insert into user_resource (user_id, resource_id, enter_central_time)
+values (1, 1, CURRENT_TIMESTAMP);
+
+insert into user_resource (user_id, resource_id, enter_central_time)
+values (1, 2, CURRENT_TIMESTAMP);
+
+commit;
